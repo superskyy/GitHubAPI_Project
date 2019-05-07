dcfb8e7e8b2452bb80c5c73b9d9d6c0dd27df420
Peter <psynevskyy.com>
2019-05-07 00:59:17 +0000

    added secrets.js to .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -11,7 +11,16 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //        .pipe(fs.createWriteStream('./future.jpg'));
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
