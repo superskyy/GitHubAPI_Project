dfbae14fcadfec0c6637e719f0856b08d322d97f
Peter <psynevskyy.com>
2019-05-07 00:54:48 +0000

    creating a function and function call

--- download_avatars.js
+++ download_avatars.js
@@ -9,3 +9,12 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //          console.log('Response Status Code: ', response.statusCode + " " + response.statusMessage);
 //        })
 //        .pipe(fs.createWriteStream('./future.jpg'));
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
